d5e8d89835b71094c79ed36a69a14bf13ecaa450
PraveenGottala <praveen.gottala@gmail.com>
2018-02-18 21:59:26 -0500

    slider app code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { EventSliderComponent } from './event-slider/event-slider.component';
+import { DummyServiceService } from './dummy-service.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventSliderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [DummyServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
